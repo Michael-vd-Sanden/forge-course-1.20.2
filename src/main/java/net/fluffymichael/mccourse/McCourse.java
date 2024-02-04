2b508698726e0fb18b37bd3c278427823bfe0c32
Michael van der Sanden <michael.mvdsanden@gmail.com>
2024-02-04 14:46:37 +0100

    Added first 2 items

--- src/main/java/net/fluffymichael/mccourse/McCourse.java
+++ src/main/java/net/fluffymichael/mccourse/McCourse.java
@@ -1,6 +1,10 @@
 package net.fluffymichael.mccourse;
 
 import com.mojang.logging.LogUtils;
+import net.fluffymichael.mccourse.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.level.ColorResolver;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -24,23 +28,17 @@ public class McCourse
     // Define mod id in a common place for everything to reference
     //has to be lowercase!
     public static final String MODID = "mccourse";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public McCourse()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the commonSetup method for modloading
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
@@ -57,20 +55,21 @@ public class McCourse
         Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-
+    {//Add an item to the creative tab
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) //an existing tab
+        {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event)
     {
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
---
+++ src/main/java/net/fluffymichael/mccourse/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.fluffymichael.mccourse.item;
+
+import net.fluffymichael.mccourse.McCourse;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, McCourse.MODID);
+
+    //A singular item, the name in the ITEMS.register has to be lowercase
+    public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
+            () -> new Item(new Item.Properties()));
+
+    public static  void register(IEventBus eventbus)
+    {
+        ITEMS.register(eventbus);
+    }
+}
